300e0913698cdd1ff273377fb5dab5676f44ba4c
tfavro <wetr9902@gmail.com>
2024-11-11 16:11:09 -0600

    updating brackets

--- src/components/Bracket.js
+++ src/components/Bracket.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import BracketImage from '../assets/images/TournamentConsolationBracket.png';
+//import BracketImage from '../assets/images/TournamentConsolationBracket.png';
+import peeweeABracket from '../assets/images/2024peeweeA.png'
+import peeweeCBracket from '../assets/images/2024peeweeC.png'
 
 const Bracket = () => {
   return (
     <div className='bracket-container'>
-      <img src={BracketImage} alt="Tournament Consolation Bracket" />
+
+        
+       <img src={peeweeABracket} alt="Tournament Peewee A Bracket" />
+    
+       
+       <img src={peeweeCBracket} alt="Tournament Peewee A Bracket" />
+ 
+     
     </div>
   );
 };
