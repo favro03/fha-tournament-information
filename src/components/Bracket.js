77f43d7300941b2ad0ecabcde10e36482f1c946c
tfavro <wetr9902@gmail.com>
2024-11-19 17:17:24 -0600

    added squirt B bracket

--- src/components/Bracket.js
+++ src/components/Bracket.js
@@ -2,6 +2,7 @@ import React from 'react';
 //import BracketImage from '../assets/images/TournamentConsolationBracket.png';
 import peeweeABracket from '../assets/images/2024peeweeA.png'
 import peeweeCBracket from '../assets/images/2024peeweeC.png'
+import squirtBBracket from '../assets/images/2024SquirtB.png'
 
 const Bracket = () => {
   return (
@@ -12,6 +13,8 @@ const Bracket = () => {
     
        
        <img src={peeweeCBracket} alt="Tournament Peewee A Bracket" />
+
+       <img src={squirtBBracket} alt="Tournament Squirt B Bracket" />
  
      
     </div>
