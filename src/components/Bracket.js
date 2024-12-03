3959ff4c2de7a065294b91f5ac01acb4955ece89
tfavro <wetr9902@gmail.com>
2024-12-02 21:31:11 -0600

    updating b bracket

--- src/components/Bracket.js
+++ src/components/Bracket.js
@@ -2,7 +2,8 @@ import React from 'react';
 //import BracketImage from '../assets/images/TournamentConsolationBracket.png';
 // import peeweeABracket from '../assets/images/2024peeweeA.png'
 // import peeweeCBracket from '../assets/images/2024peeweeC.png'
-import squirtBBracket from '../assets/images/2024SquirtB.png'
+// import squirtBBracket from '../assets/images/2024SquirtB.png'
+import squirtBBracket from '../assets/images/squartBBracket.png'
 
 const Bracket = () => {
   return (
@@ -14,7 +15,7 @@ const Bracket = () => {
        
        <img src={peeweeCBracket} alt="Tournament Peewee A Bracket" /> */}
 
-       <img src={squirtBBracket} alt="Tournament Squirt B Bracket" />
+       <img style={{marginTop: '20px', marginBottom: '20px'}} src={squirtBBracket} alt="Tournament Squirt B Bracket" />
  
      
     </div>
