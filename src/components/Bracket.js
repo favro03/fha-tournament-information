a190106a4a72bb177259922ecab564a8bce37451
tfavro <wetr9902@gmail.com>
2025-01-07 18:25:08 -0600

    updating brackets

--- src/components/Bracket.js
+++ src/components/Bracket.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import U15ABracket from '../assets/images/U15A.png'
-import U15BBracket from '../assets/images/U15B.png'
-import U12B1Bracket from '../assets/images/U12B1.png'
-import U12B2Bracket from '../assets/images/U12B2.png'
+
 import miniMite from '../assets/images/miniMite.png'
 
 const Bracket = () => {
@@ -11,11 +8,6 @@ const Bracket = () => {
       <h2 style={{ marginTop: '30px', fontWeight: 'bold' }}>Brackets</h2>
 
 
-          
-      <img className='marginBottom' src={U15ABracket} alt="Tournament U15A Bracket" />
-      <img className='marginBottom' src={U15BBracket} alt="Tournament U15B Bracket" />
-      <img className='marginBottom' src={U12B1Bracket} alt="Tournament U12B1 Bracket" /> 
-      <img className='marginBottom' src={U12B2Bracket} alt="Tournament U12B2 Bracket" />
       <img className='marginBottom' src={miniMite} alt="Tournament Mini Mite Bracket" />
          
    
